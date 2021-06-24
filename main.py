83ea5019cacaa6797fe462c7ba3b8ee3982e4da1
HekateSK <itzshakeey@gmail.com>
2021-06-24 12:32:42 +0100

    Added DM function

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import discord
+from discord import colour
+from discord import embeds
 import requests
 import json
-import random
-
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord import DMChannel
 
 load_dotenv()
 
-#Stores bot within client variable
-client = discord.Client()
-
-sad_words = ["sad", "depressed", "unhappy", "miserable", "depressing"]
-starter_encouragements = ["Cheer up!", "Hang in there!", "You are a great person / bot!"]
+#client = discord.Client()
+#intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client = commands.Bot(command_prefix = '.')
 
 
-intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-#client = commands.Bot(command_prefix = '*', intents = intents)
+#Status
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'
+    .format(client))
+
+
+#Join
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
 
 #get a quote
 def get_quote():
@@ -25,11 +34,6 @@ def get_quote():
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return(quote)
 
-#uptime status
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'
-    .format(client))
 
 #specific keyword prompt
 @client.event
@@ -45,9 +49,34 @@ async def on_message(message):
     
     if msg.startswith('test'):
         await message.channel.send('I am fully functional!')
+        
 
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
+#DM
+@client.command(name='dmsend', pass_context=True)
+async def dmsend(ctx):
+    user = await client.fetch_user("228950942337335298")
+    await DMChannel.send(user, "stop be gay!")
 
+'''
+#embeds
+@client.command()
+async def displayembed():
+    embed = discord.Embed(
+        title = 'Title',
+        description = 'The description',
+        colour = discord.colour.blue()
+    )
+
+    embed.set_footer(text='The footer')
+    embed.set_image(url='https://cdn.discordapp.com/attachments/784906587567292446/856873777937907742/Jojolion_.png')
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/784906587567292446/856873777937907742/Jojolion_.png')
+    embed.set_author(name='Author Name', 
+    icon_url='https://cdn.discordapp.com/attachments/784906587567292446/856873777937907742/Jojolion_.png')
+    embed.add_field(name='Field', value='Field value', inline=False)
+    embed.add_field(name='Field', value='Field value', inline=True)
+    embed.add_field(name='Field', value='Field value', inline=True)
+
+    await client.say(embed=embed)
+'''
 
 client.run(os.environ.get("SECRET_KEY"))
