29b060f78661546334244a7eeb82a06a108ec54f
HekateSK <itzshakeey@gmail.com>
2021-07-04 14:03:37 +0100

    setting up embeds

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,7 @@
-from discord import Intents
+from discord import Intents, channel
 from apscheduler.schedulers.asyncio import  AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
+from discord import Embed
 
 PREFIX = "+"
 OWNER_IDS = [114719310819098629]
@@ -39,10 +40,20 @@ class client(BotBase):
             self.ready = True
             print("Bot ready.")
             
+            channel = self.get_channel(856241227997642773)
+            await channel.send("Now online")
+
+            embed = Embed(title="Now online!", description="Shakeey")
+            fields = [("Name", "Value", True),
+            ("Another field", "This field is next to the other one.", True), 
+            ("A non-inline field", "This field will appear on its own row", False)]
+            
+            for name, value, inline in fields:
+                embed.add_field(name="Name", value="Value", inline=True)
+            await channel.send(embed=embed)
         
         else:
             print("Bot Reconnected")
-
 
     async def on_message(self, message):
         pass
