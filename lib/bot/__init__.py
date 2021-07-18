2a2d8dc3ae3c4defefee004b6204a866e1cdf241
HekateSK <itzshakeey@gmail.com>
2021-07-18 12:30:53 +0100

    removed db, optimised cogs, major restruction

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,4 +1,5 @@
 from glob import glob
+from time import sleep
 
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -11,8 +12,6 @@ from discord.ext.commands.errors import CommandNotFound
 
 from discord import Intents, client
 from discord import Embed, File
-
-from ..db import db
 import random, os
 
 
@@ -22,33 +21,33 @@ PREFIX = '+'
 OWNER_IDS = [114719310819098629]
 COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")]
 
-class Ready(object):
-    def __init__(self):
-        for cog in COGS:
-            setattr(self, cog, False)
+# class Ready(object):
+#     def __init__(self):
+#         for cog in COGS:
+#             setattr(self, cog, False)
 
-    def ready_up(self, cog):
-        setattr(self, cog, True)
-        #print(f"{cog} cog ready")
+#     def ready_up(self, cog):
+#         setattr(self, cog, True)
+#         print(f"{cog} cog ready")
     
-    def all_ready(self):
-        return all([getattr(self, cog) for cog in COGS])
+#     def all_ready(self):
+#         return all([getattr(self, cog) for cog in COGS])
 
 class bot(BotBase):
     def __init__(self):
         self.PREFIX = PREFIX
         self.ready = False
-        self.cogs_ready = Ready()
+        #self.cogs_ready = Ready()
         self.guild = None
         self.scheduler = AsyncIOScheduler()
 
-        db.autosave(self.scheduler)
         super().__init__(command_prefix=PREFIX, OWNER_IDS=OWNER_IDS, intents=Intents.all())
     
     def setup(self):
         for cog in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
             print(f"{cog} cog loaded")
-
+        
 
     def run(self, version):
         self.VERSION = version
@@ -58,12 +57,12 @@ class bot(BotBase):
 
         with open("./lib/bot/token", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
-        #print("Starting up bot...")
         super().run(self.TOKEN, reconnect=True)
 
 
     async def on_connect(self):
-        print("Systems ready!")
+            print('We have logged in as {0.user}'
+            .format(bot))
 
     async def on_disconnect(self):
         print("Bot has disconnected.")
@@ -84,31 +83,12 @@ class bot(BotBase):
 
     async def on_ready(self):
         if not self.ready:
-            
             self.stdout = self.get_channel(856241227997642773)
-            # self.scheduler.add_job(self.rules_reminder, CronTrigger(day_of_week=0, hour=12, minute=0, second=0))
-            # self.scheduler.start()
-
-            # self.update_db()
-            
-            # embed = Embed(title="Now online!", description="Shakeey",
-            # colour=0xFF0000, )
-            # fields = [("Name", "Value", True),
-            # ("Another field", "This field is next to the other one.", True),
-            # ("A non-inline field", "This field will appear on its own row", False)]
-            # for name, value, inline in fields:
-            #     embed.add_field(name=name, value=value, inline=inline)
-            # await channel.send(embed=embed)
-
-
-            # while not self.cogs_ready.all_ready():
-            #     await sleep(0.5)
 
             await self.stdout.send("Now online!")
             
             self.ready = True
-            print('We have logged in as {0.user}'
-            .format(bot))
+            print('System Ready!')
 
             # await self.stdout.send("Ora ora ora!")
             # await self.stdout.send(file=File("data\jojo\image1.gif"))
--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -6,21 +6,9 @@ class Fun(Cog):
 
     @Cog.listener()
     async def on_ready(self):
-        if not self.bot.ready:
-            self.bot.cogs_ready.ready_up("fun")
-            
-        await self.bot.stdout.send("Cogs are in motion")
-    
-    @Cog.event()
-    async def on_message(message):
-
-        msg = message.content
-
-        if msg.startswith('test'):
-            await message.channel.send('I am fully functional!')
-
+        await self.bot.stdout.send("fun cog ready")
+        print("Fun cog ready")
 
 def setup(bot):
     bot.add_cog(Fun(bot))
 
-#cogs sucks
