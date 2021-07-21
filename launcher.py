348698826700817ee5e557905adbcdb5341ed3f2
HekateSK <itzshakeey@gmail.com>
2021-07-21 21:53:24 +0100

    oop main method

--- launcher.py
+++ launcher.py
@@ -1,10 +1,9 @@
 import os
 from discord import client
-from dotenv.main import load_dotenv
-from lib.bot import bot
+from lib.bot import main
 
 VERSION = "0.0.5"
 
 #bot.run(VERSION)
-load_dotenv()
-client.run(os.environ.get("SECRET_KEY"))
+
+client.run()
